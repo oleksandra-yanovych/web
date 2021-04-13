a93fc08e57cd6d1fc950ef073fd49fdba5c6b4ff
aleksasha <61113578+oleksandra-yanovych@users.noreply.github.com>
2021-04-13 20:18:57 +0300

    LoveCoffeBar

---
+++ js/menu.js
@@ -0,0 +1,5 @@
+$('.button_menu_mobile').on('click', function() {
+	$('.burger').toggleClass('burger_open');
+});
+/*просто скріпт на JQuery який шукає у документі клас button_menu_mobile, 
+і визначає коли на нього нажимають "click" і після чого змінює клас burger на burger_open */
